5637d6450f775e1d4d26de8a6cce0cce4c7f761a
dominicjunik <111475701+dominicjunik@users.noreply.github.com>
2023-08-07 14:57:41 -0600

    added all 3 searches

---
+++ models/fruits.js
@@ -0,0 +1,35 @@
+const fruits = [
+    {
+        name:'apple',
+        color: 'red',
+        readyToEat: true
+    },
+    {
+        name:'pear',
+        color: 'green',
+        readyToEat: false
+    },
+    {
+        name:'banana',
+        color: 'yellow',
+        readyToEat: true
+    },
+    {
+        name:'kiwi',
+        color: 'green',
+        readyToEat: false
+    },
+    {
+        name:'plum',
+        color: 'purple',
+        readyToEat: true
+    },
+    {
+        name:'mango',
+        color: 'greenish orange',
+        readyToEat: false
+    },
+];
+
+// oldschool way to export using commonJS
+module.exports = fruits;
---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = 8080
+
+const fruits = require('./models/fruits')
+
+// example of an "index" route
+app.get('/fruits', (req, res) => {
+    res.send(fruits)
+})
+
+app.get('/filter', (req, res) => {
+    console.log(req.query)
+    let filterArray = []
+    Boolean('false')
+    if (req.query.readyToEat) {
+        let edible
+        if (req.query.readyToEat === 'false'){
+            edible = false
+        } else {edible = true}                
+       
+        let edibleFruits = fruits.filter((fruit) => fruit.readyToEat === edible)
+        console.log(edibleFruits)
+        filterArray = [...edibleFruits]        
+    }
+
+    if (req.query.name) {        
+        if (req.query.readyToEat === 'false' || req.query.readyToEat === 'true') {
+            filterArray = filterArray.filter((fruit) => fruit.name.includes(req.query.name))
+            
+        } else {
+            filterArray = fruits.filter((fruit) => fruit.name.includes(req.query.name))            
+        }
+    }
+
+    if (req.query.color) {
+        if (req.query.readyToEat === 'false' || req.query.readyToEat === 'true' || req.query.name) {
+            filterArray = filterArray.filter((fruit) => fruit.color.includes(req.query.color))
+        } else {
+            filterArray = fruits.filter((fruit) => fruit.color.includes(req.query.color))    
+        }
+    }
+
+    res.send(filterArray)
+
+   
+})
+
+//example of a "show" route
+app.get('/fruits/:indexOfFruit', (req, res) => {
+    let fruit = fruits[req.params.indexOfFruit]
+    if (fruit) {
+        res.send(fruit)
+    } else { 
+        res.send('<h1>ERROR 404: fruit not found :)</h1>')
+    }
+    // fancy version with an or || statement instead
+    // res.send(fruits[req.params.indexOfFruit] || '<h1>ERROR 404: fruit not found :)</h1>')
+})
+
+app.get('/', (req, res) => {
+    res.send('plant home page')
+})
+
+app.listen(PORT, () => {
+    console.log('Listening on port: ' + PORT)
+})
+
+/*
+create a route that uses the request queries from the client to filter the fruit data in someway
+
+HINTS: use .filter(), .includes()
+
+Ex. http://localhost:8080/fruits/filter?color=a&readyToEat=true
+
+if (req.query.name.includes())
+*/
